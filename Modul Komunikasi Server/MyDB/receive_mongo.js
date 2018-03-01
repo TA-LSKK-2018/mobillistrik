a47475043e1ae9b242a33d58aa5a093ddfbc39a0
gabriellailena <36254239+gabriellailena@users.noreply.github.com>
2018-03-01 19:01:03 +0700

    Update receive_mongo.js

--- Modul Komunikasi Server/MyDB/receive_mongo.js
+++ Modul Komunikasi Server/MyDB/receive_mongo.js
@@ -3,30 +3,61 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var amqp = require('amqplib/callback_api');
-var vhost = 'amqp://localhost/mobilistrik';
+var vhost = 'amqp://localhost//mobillistrik';
+//'amqp://mobillistrik:mobillistrik@167.205.66.196:5672/%2fmobillistrik?heartbeat=10'
 
 //Connect to the amqp broker
 amqp.connect(vhost, function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'sensor';
-	var routekey = 'mobilistrik.sensor';
+    var q = 'dataSensor';
+	var routekey = 'mobillistrik.sensor';
 
     ch.assertQueue(q, {durable: true});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
 
     ch.consume(q, function(msg) {
       console.log("Incoming msg : "+msg.content.toString());
+	  
 	  var myobj = JSON.parse(msg.content.toString());
 	  
-	  //Connect to the database and insert consumed data as a document
-	  MongoClient.connect(url, function(err, db) {
+	  var buffer = {};
+	  var id_ref = 1;
+	  var id_in = parseInt(myobj.id_ind.toString(), 10);
+	  
+	  for (var dataLength = 0; dataLength < 4; dataLength++) {
+		  if (id_ref == id_in) {
+			buffer += myobj + " ";
+		  }
+	  }
+	  
+	  id_ref += 1;
+	  
+	  for (var i = 0; i < myobj.length; i++) {
+				console.log(buffer[i]);
+				id_ref++;
+			}
+	  	
+		//Connect to the database and insert consumed data as a document	
+		MongoClient.connect(url, function(err, db) {
+		var dbo = db.db("EVOI");
+		
 		if (err) throw err;
-		var dbo = db.db("mydb");
-		dbo.collection("sensors").insertOne(myobj, function(err, result) {
-			if (err) throw err;
-			console.log("1 document inserted.");
+		
+			dbo.collection("sensors").insertOne(myobj, function(err, result) {
+				if (err) throw err;
+				console.log("1 document inserted.");
 			});
-		});	  
+			
+		/*	//If existing document has the same id_ind as the received message, then push the message into the document
+			var query = { id_ind: { $eq: myobj.id_ind } };
+			var newvalues = { $push: myobj };
+			
+			dbo.collection("sensors").updateOne( query, newvalues, function(err, res) {
+				if (err) throw err;
+				console.log("1 document updated.");
+			});
+		*/
+	    });
     }, {noAck: true});
   });
 });
