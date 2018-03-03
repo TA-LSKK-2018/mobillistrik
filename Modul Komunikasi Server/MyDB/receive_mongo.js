095a2ddc38479f270877734e15f314960c3d7772
gabriellailena <36254239+gabriellailena@users.noreply.github.com>
2018-03-03 23:31:25 +0700

    Update receive_mongo.js

--- Modul Komunikasi Server/MyDB/receive_mongo.js
+++ Modul Komunikasi Server/MyDB/receive_mongo.js
@@ -16,38 +16,41 @@ amqp.connect(vhost, function(err, conn) {
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
 
     ch.consume(q, function(msg) {
-      console.log("Incoming msg : "+msg.content.toString());
+      console.log("Message received.");
 	  
 	  var myobj = JSON.parse(msg.content.toString());
 	  
-	  var buffer = {};
+	  var buffer = [];
 	  var id_ref = 1;
-	  var id_in = parseInt(myobj.id_ind.toString(), 10);
+	  var id_in = parseInt(myobj.id_ind, 10);
+	  var flag = 1;
 	  
-	  for (var dataLength = 0; dataLength < 4; dataLength++) {
-		  if (id_ref == id_in) {
-			buffer += myobj + " ";
-		  }
-	  }
 	  
-	  id_ref += 1;
-	  
-	  for (var i = 0; i < myobj.length; i++) {
+	  if (id_ref == id_in) {
+			buffer.push(myobj);
+			flag += 1;
+		}
+	 
+	 if (flag == 4) {
+		id_ref += 1;
+		console.log(id_ref);
+	}
+	 
+		for (var i = 0; i < buffer.length; i++) {
 			console.log(buffer[i]);
-	  }
-	  	
+		}
 		//Connect to the database and insert consumed data as a document	
-		MongoClient.connect(url, function(err, db) {
+		/*MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("EVOI");
 		
 		if (err) throw err;
 		
-			dbo.collection("sensors").insertOne(myobj, function(err, result) {
+			dbo.collection("sensors").insertOne(buffer, function(err, result) {
 				if (err) throw err;
 				console.log("1 document inserted.");
 			});
 			
-		/*	//If existing document has the same id_ind as the received message, then push the message into the document
+			//If existing document has the same id_ind as the received message, then push the message into the document
 			var query = { id_ind: { $eq: myobj.id_ind } };
 			var newvalues = { $push: myobj };
 			
@@ -55,8 +58,8 @@ amqp.connect(vhost, function(err, conn) {
 				if (err) throw err;
 				console.log("1 document updated.");
 			});
-		*/
-	    });
+		
+	    }); */
     }, {noAck: true});
   });
 });
